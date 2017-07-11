8f27a69a36232ecd02394ce88f5a1edcf5499e79
Demavend <demavend12345@gmail.com>
2017-07-11 08:15:28 +0300

    fix&add content

---
+++ index.js
@@ -0,0 +1,11 @@
+<script> function jv() {
+	$('#jv').modal();}
+	function dl() {
+	$('#dl').modal();}
+	function ratt() {
+	$('#ratt').modal();}
+	function witch() {
+	$('#witch').modal();}
+	function nord() {
+	$('#nord').modal();}
+</script>
