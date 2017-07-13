4eebdaa34604e6549d0f8c933a306ec64da7174a
Demavend <demavend12345@gmail.com>
2017-07-13 16:32:51 +0300

    fix spoiler

--- index.js
+++ index.js
@@ -20,14 +20,31 @@ $('#hide1').hide();
 	$(document).ready(function() { 
     
     var overlay = $('#overlay'); 
-    var show_hide = $('li.show_hide'); 
+    var show_hide = $('a.show_hide'); 
     
     show_hide.click( function(event){ 
         event.preventDefault();
-        var li = $(this).attr('href'); 
+        var a = $(this).attr('name'); 
         overlay.fadeIn(100, 
             function(){ 
-                $(li).hide();
+                $(a).hide();
+         });
+     });
+
+     
+});		
+
+$(document).ready(function() { 
+    
+    var overlay = $('#overlay'); 
+    var show = $('a.dropdown-footer'); 
+    
+    show.click( function(event){ 
+        event.preventDefault();
+        var a1 = $('div[href^="#modal"]'); 
+        overlay.fadeIn(100, 
+            function(){ 
+                $(a1).show();
          });
      });
 
