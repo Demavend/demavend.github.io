e7872f4d97614a990b0fb6428fc967461921473b
Demavend <demavend12345@gmail.com>
2017-08-03 11:32:23 +0300

    small fix btn

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ var booksElements = [];
 var myModal = document.querySelectorAll('.openModal');  	                    //Все кнопки модалок
 var newDiv;
 
+
 var innerDiv = function(data, i){
 	newDiv = document.createElement('div');
-	newDiv.innerHTML = '<div class="container"><div class="col"><h1 class="text-center text-primary">'+data.items[i].volumeInfo.title+'</h1><div class="row bg-success"><div class="col-md-2 col-sd-2" style="text-align:center;"><img src="'+data.items[i].volumeInfo.imageLinks.thumbnail+'" width="100%" height="170" class="img-responsive"></img></div><div class="col-md-8 col-sd-8"><h4 class="text-justify">'+data.items[i].volumeInfo.description+'</h4></div><div class="col-md-2 col-sd-2" style="text-align:center;line-height:15em;"><button class="btn btn-info openModal" name='+i+'  >Показать описание</button></div></div></div></div>';
+	newDiv.innerHTML = '<div class="container"><div class="col"><h1 class="text-center text-primary">'+data.items[i].volumeInfo.title+'</h1><div class="row bg-success"><div class="col-md-2 col-sd-2" style="text-align:center;"><img src="'+data.items[i].volumeInfo.imageLinks.thumbnail+' "width="100%"  class="img-responsive"></img></div><div class="col-md-8 col-sd-8"><h4 class="text-justify">'+data.items[i].volumeInfo.description+'</h4></div><div class="col-md-2 col-sd-2" style="text-align:center;margin-top:90px;"><button class="btn btn-info openModal" name='+i+'  >Показать<br>описание</button></div></div></div></div>';
 	document.body.appendChild(newDiv);
 };
+
 
 $.ajax({
 	url:"https://www.googleapis.com/books/v1/volumes?q=javascript&orderBy=newest&startIndex=0&maxResults=10",
@@ -15,10 +17,11 @@ $.ajax({
 		booksElements.push(data);
 		for(var i=0; i < booksElements[0].items.length; i++){
 		innerDiv(data, i);
-	};
+		};
 	},
 	type:"GET"
 });
+
 
 function isEmpty(obj) {
 	for (var key in obj) {
