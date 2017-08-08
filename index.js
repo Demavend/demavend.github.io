a0e74b84dad01fdba4c617c8da84df7f3d48b15e
Demavend <demavend12345@gmail.com>
2017-08-08 17:14:49 +0300

    cut some code

--- index.js
+++ index.js
@@ -1,45 +1,58 @@
-var booksElements = [];
-var myModal = document.querySelectorAll('.openModal');  	                    //Все кнопки модалок
-var newDiv;
-
-
-var innerDiv = function(data, i){
-	newDiv = document.createElement('div');
-	newDiv.innerHTML = '<div class="container"><div class="col"><h1 class="text-center text-primary">'+data.items[i].volumeInfo.title+'</h1><div class="row bg-success"><div class="col-md-2 col-sd-2" style="text-align:center;"><img src="'+data.items[i].volumeInfo.imageLinks.thumbnail+' "width="100%"  class="img-responsive"></img></div><div class="col-md-8 col-sd-8"><h4 class="text-justify">'+data.items[i].volumeInfo.description+'</h4></div><div class="col-md-2 col-sd-2" style="text-align:center;margin-top:90px;"><button class="btn btn-info openModal" name='+i+'  >Показать<br>описание</button></div></div></div></div>';
-	document.body.appendChild(newDiv);
+let items = [];
+let newDiv;
+const url = 'https://www.googleapis.com/books/v1/volumes?q='+
+'javascript&orderBy=newest&startIndex=0&maxResults=10';
+const LAST = document.querySelector('#last');
+const createBlock = function(data, i) {
+    newDiv = document.createElement('div');
+    newDiv.innerHTML = '<div class="container"><div class="col"><h1 class="text-center text-primary">' +
+        data.title +
+        '</h1><div class="row bg-success"><div class="col-md-2"><img src="' +
+        data.imageLinks.thumbnail +
+        '"width="100%"  class="img-responsive"></img></div><div class="col-md-8"><h4 class="text-justify">' +
+        data.description +
+        '</h4></div><div class="col-md-2"><button class="btn btn-info openModal" name=' +
+        i + '>Show<br>summary</button></div></div></div></div>';
+    document.body.insertBefore(newDiv, LAST);
 };
 
 
 $.ajax({
-	url:"https://www.googleapis.com/books/v1/volumes?q=javascript&orderBy=newest&startIndex=0&maxResults=10",
-	dataType:"json",
-	success: function(data){
-		booksElements.push(data);
-		for(var i=0; i < booksElements[0].items.length; i++){
-		innerDiv(data, i);
-		};
-	},
-	type:"GET"
+    url,
+    dataType: "json",
+    success: (data) => {
+        for (var i = 0; i < data.items.length; i++) {
+            items.push(data.items[i].volumeInfo);
+            createBlock(items[i], i);
+        };
+    },
+    type: "GET"
 });
 
 
-function isEmpty(obj) {
-	for (var key in obj) {
-		return obj;
-	};
-	obj='Неизвесно';
-	return obj;
+function fillingOfVoids(obj) {
+    if (obj) {
+        return obj;
+    } else {
+        obj = 'Unknown';
+        return obj;
+    }
 };
 
 
-function windowContent (x){                                                		//Функция для вставки
-	document.querySelector('#bodyModal').innerHTML =
-	"Автор: "+isEmpty(x.volumeInfo.authors)+"<br>Название: "+isEmpty(x.volumeInfo.title)+"<br>Категория жанра: "+isEmpty(x.volumeInfo.categories)+"<br>Издатель: "+isEmpty(x.volumeInfo.publisher)+"<br>Дата публикации: "+isEmpty(x.volumeInfo.publishedDate);}       //текста в модалку
+function modalContent(src) {
+    document.querySelector('#bodyModal').innerHTML =
+        'Author: ' + fillingOfVoids(src.authors) + '<br>Title: ' +
+        fillingOfVoids(src.title) + '<br>Category: ' +
+        fillingOfVoids(src.categories) + '<br>Publisher: ' +
+        fillingOfVoids(src.publisher) + '<br>Published date: ' +
+        fillingOfVoids(src.publishedDate);
+}
 
-document.body.addEventListener('click', function(e){                    		//Ловим клик по бади
-	if (e.target.getAttribute('class') === 'btn btn-info openModal'){   		//Кнопка открывает модалку?
-	var a = e.target.getAttribute('name');                                    	//Узнаем какой текст в окне
-	windowContent(booksElements[0].items[a]);                                        	//Вставляем текст
-	$('#modal').modal();                                                    	//Открываем окно
-	}
+document.body.addEventListener('click', function(e) {
+    if (e.target.getAttribute('class') === 'btn btn-info openModal') {
+        var a = e.target.getAttribute('name');
+        modalContent(items[a]);
+        $('#modal').modal();
+    }
 }, true);
