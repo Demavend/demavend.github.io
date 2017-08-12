e84d82753d9d7c3d7dc844fc58a9cdbc7f9136e6
Demavend <demavend12345@gmail.com>
2017-08-12 14:18:34 +0300

    Filling the page with content from object

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
-const MODAL_HEADER = `<button class="close" data-dismiss="modal">x
+const MODAL_TOP = `<button class="close" data-dismiss="modal">x
 </button><h4 class='text-center text-primary'>`;
-const TICKETS = ['Author: ', '<br>Title: ', '<br>Category: ', '<br>Publisher: ', '<br>Published date: '];
 const url = 'https://www.googleapis.com/books/v1/volumes?q=' +
     'javascript&orderBy=newest&startIndex=0&maxResults=10';
 const END = document.querySelector('#end');
-const createBlock = (data, i) => {
-    let newDiv = document.createElement('div');
-    newDiv.innerHTML = `<div class="container"><div class="col"><h1 class="text-center text-primary">${data.title}` +
-        `</h1><div class="row bg-success"><div class="col-md-2"><img src="${data.imageLinks.thumbnail}` +
-        `"width="100%"  class="img-responsive"></img></div><div class="col-md-8"><h4 class="text-justify">${data.description}` +
-        `</h4></div><div class="col-md-2"><button class="btn btn-info openModal" name=${i}` +
-        `>Show<br>summary</button></div></div></div></div>`;
-    document.body.insertBefore(newDiv, END);
+const createBlock = (data) => {
+    for (let key in data) {
+        let newDiv = document.createElement('div');
+        newDiv.innerHTML = `<div class="container"><div class="col"><h1 class="text-center text-primary">${data[key].Summary.Title}` +
+            `</h1><div class="row bg-success"><div class="col-md-2"><img src="${data[key].Img}` +
+            `"width="100%"  class="img-responsive"></img></div><div class="col-md-8"><h4 class="text-justify">${data[key].Description}` +
+            `</h4></div><div class="col-md-2"><button class="btn btn-info openModal" id=${key}` +
+            `>Show<br>summary</button></div></div></div></div>`;
+        document.body.insertBefore(newDiv, END);
+    }
 };
 let books = {};
-
 $.ajax({
     url,
     dataType: 'json',
     success: (data) => {
-        console.log(data);
         for (let i = 0; i < data.items.length; i++) {
             let {
                 etag: id,
@@ -35,50 +34,32 @@ $.ajax({
                     }
                 }
             } = data.items[i];
-            books[id] = {
+            books[id] = {'Summary':{
                 'Title': title || 'Unknown',
                 'Author': authors || 'Unknown',
                 'Category': categories || 'Unknown',
                 'Publisher': publisher || 'Unknown',
-                'Published date': publishedDate || 'Unknown',
+                'Published date': publishedDate || 'Unknown'},
                 'Description': description || 'Unknown',
                 'Img': img || 'Unknown'
             };
-            //console.log(books);
-            //items.push(data.items[i].volumeInfo);
-            //createBlock(items[i], i);
         };
-        console.dir(books);
+        createBlock(books);
     },
     type: 'GET'
 });
-
-class Book {
-    constructor(src) {
-        this.data = [src.authors, src.title, src.categories, src.publisher, src.publishedDate];
-    }
-    fillingModal() {
-        let content = '';
-        this.data.forEach((item, i, arr) => {
-            if (item === undefined) {
-                item = 'Unknown';
-            };
-            content += TICKETS[i] + item;
-        })
-        return content;
-    };
-};
-
 function modalContent(src) {
-    document.querySelector('.modal-header').innerHTML = `${MODAL_HEADER}${src.title}</h4>`;
-    let book = new Book(src);
-    document.querySelector('#bodyModal').innerHTML = book.fillingModal();
-}
-
+    document.querySelector('.modal-header').innerHTML = `${MODAL_TOP}${src.Title}</h4>`;
+    let book='';
+    for (let key in src) {
+        book += `${key}:  ${src[key]};<br>`;
+    };
+    document.querySelector('#bodyModal').innerHTML = book;
+};
 document.body.addEventListener('click', (e) => {
     if (e.target.getAttribute('class') === 'btn btn-info openModal') {
-        var a = e.target.getAttribute('name');
-        modalContent(items[a]);
+        let id = e.target.id;
+        modalContent(books[id].Summary);
         $('#modal').modal();
     }
 }, true);
