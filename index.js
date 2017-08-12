06542252dbeefdbae5655f0968cd03e416f96746
Demavend <demavend12345@gmail.com>
2017-08-12 13:36:51 +0300

    Get data with destructing assignment

--- index.js
+++ index.js
@@ -1,31 +1,56 @@
-let items = [];
-let newDiv;
 const MODAL_HEADER = `<button class="close" data-dismiss="modal">x
 </button><h4 class='text-center text-primary'>`;
 const TICKETS = ['Author: ', '<br>Title: ', '<br>Category: ', '<br>Publisher: ', '<br>Published date: '];
 const url = 'https://www.googleapis.com/books/v1/volumes?q=' +
     'javascript&orderBy=newest&startIndex=0&maxResults=10';
-const LAST = document.querySelector('#last');
+const END = document.querySelector('#end');
 const createBlock = (data, i) => {
-    newDiv = document.createElement('div');
+    let newDiv = document.createElement('div');
     newDiv.innerHTML = `<div class="container"><div class="col"><h1 class="text-center text-primary">${data.title}` +
         `</h1><div class="row bg-success"><div class="col-md-2"><img src="${data.imageLinks.thumbnail}` +
         `"width="100%"  class="img-responsive"></img></div><div class="col-md-8"><h4 class="text-justify">${data.description}` +
         `</h4></div><div class="col-md-2"><button class="btn btn-info openModal" name=${i}` +
         `>Show<br>summary</button></div></div></div></div>`;
-    document.body.insertBefore(newDiv, LAST);
+    document.body.insertBefore(newDiv, END);
 };
+let books = {};
 
 $.ajax({
     url,
-    dataType: "json",
+    dataType: 'json',
     success: (data) => {
-        for (var i = 0; i < data.items.length; i++) {
-            items.push(data.items[i].volumeInfo);
-            createBlock(items[i], i);
+        console.log(data);
+        for (let i = 0; i < data.items.length; i++) {
+            let {
+                etag: id,
+                volumeInfo: {
+                    title,
+                    categories,
+                    publisher,
+                    publishedDate,
+                    authors,
+                    description,
+                    imageLinks: {
+                        thumbnail: img,
+                    }
+                }
+            } = data.items[i];
+            books[id] = {
+                'Title': title || 'Unknown',
+                'Author': authors || 'Unknown',
+                'Category': categories || 'Unknown',
+                'Publisher': publisher || 'Unknown',
+                'Published date': publishedDate || 'Unknown',
+                'Description': description || 'Unknown',
+                'Img': img || 'Unknown'
+            };
+            //console.log(books);
+            //items.push(data.items[i].volumeInfo);
+            //createBlock(items[i], i);
         };
+        console.dir(books);
     },
-    type: "GET"
+    type: 'GET'
 });
 
 class Book {
