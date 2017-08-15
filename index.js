6574590b50aa6c30274fa873aaffcc36af0ec57c
Demavend <demavend12345@gmail.com>
2017-08-15 18:57:09 +0300

    Fix repeating of books

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const STEP = 10;
 const fullUrl = (val, start, max) => {
     let url = `https://www.googleapis.com/books/v1/volumes?q=${val}
-&orderBy=newest&startIndex=${start}&maxResults=${max}`;
+&startIndex=${start}&maxResults=${max}`;
     return (url);
 };
 const clean = () => {
     $('div.panel-success').remove();
-    books = {};
+    document.querySelector('.showMore').style.display = 'none';
+    responseResult = [];
 };
 const createBlock = (data) => {
     for (let key in data) {
@@ -14,14 +15,47 @@ const createBlock = (data) => {
         newDiv.innerHTML = `<div class="panel panel-success"><div class="panel-heading"><h1 class="text-center">${data[key].summary.title}` +
             `</h1></div><div class="panel-body"><div class="col-md-2"><img src="${data[key].img}` +
             `"width="100%"  class="img-responsive"></img></div><div class="col-md-8"><h4 class="text-justify">${data[key].description}` +
-            `</h4></div><div class="col-md-2"><button class="btn btn-info openModal" id=${key}` +
+            `</h4></div><div class="col-md-2"><button class="btn btn-info openModal" id=${data[key].id}` +
             `>Show<br>summary</button></div></div></div>`;
         document.querySelector('#bookshelf').appendChild(newDiv);
     }
 };
-let books = {};
+let responseResult;
 let serch;
 let startIndex = 0;
+
+function arrangmrntBooks(data) {
+    let books = [];
+    for (let i = 0; i < data.items.length; i++) {
+        let {
+            id,
+            volumeInfo: {
+                title,
+                categories,
+                publisher,
+                publishedDate,
+                authors,
+                description,
+                imageLinks: {
+                    thumbnail: img,
+                }
+            }
+        } = data.items[i];
+        books.push({
+            id,
+            summary: {
+                title: title || 'Unknown',
+                author: authors || 'Unknown',
+                category: categories || 'Unknown',
+                publisher: publisher || 'Unknown',
+                published_date: publishedDate || 'Unknown'
+            },
+            description: description || 'Unknown',
+            img: img || 'Unknown'
+        });
+    };
+    return books;
+};
 
 function getBooks(src) {
     return new Promise((resolve, reject) => {
@@ -35,69 +69,29 @@ function getBooks(src) {
     });
 };
 
-class bookshelf {
-    constructor(data) {
-        for (let i = 0; i < data.items.length; i++) {
-            let {
-                id: id,
-                volumeInfo: {
-                    title,
-                    categories,
-                    publisher,
-                    publishedDate,
-                    authors,
-                    description,
-                    imageLinks: {
-                        thumbnail: img,
-                    }
-                }
-            } = data.items[i];
-            books[id] = {
-                summary: {
-                    title: title || 'Unknown',
-                    author: authors || 'Unknown',
-                    category: categories || 'Unknown',
-                    publisher: publisher || 'Unknown',
-                    published_date: publishedDate || 'Unknown'
-                },
-                description: description || 'Unknown',
-                img: img || 'Unknown'
-            };
-        };
-    };
-    showBlocks() {
-        if (document.querySelector('div.panel-success')) {
-            $('div.panel-success').remove();
-        } else {
-            document.querySelector('.showMore').style.display = 'block';
-        };
-        createBlock(books);
-    };
-    showMore() {
-        createBlock(books);
-    };
-};
-
 document.getElementById('btnSerch').addEventListener('click', (e) => {
     serch = document.getElementById('bookName').value;
-    books = {};
+    if (document.querySelector('div.panel-success')) {
+        clean();
+    };
     getBooks(fullUrl(serch, startIndex, STEP)).then(response => {
-        let book = new bookshelf(response);
-        book.showBlocks();
+        responseResult = arrangmrntBooks(response);
+        createBlock(responseResult);
     });
-});
-document.querySelector('.showMore').addEventListener('click', (e) => {
-    startIndex += STEP;
-    books = {};
-    getBooks(fullUrl(serch, startIndex, STEP)).then(response => {
-        let book = new bookshelf(response);
-        book.showMore();
-    });
+    document.querySelector('.showMore').style.display = 'block';
 });
 document.getElementById('clean').addEventListener('click', (e) => {
     clean();
-    document.querySelector('.showMore').style.display = 'none';
 });
+document.querySelector('.showMore').addEventListener('click', (e) => {
+    startIndex += STEP;
+    getBooks(fullUrl(serch, startIndex, STEP)).then(response => {
+        responseResult = responseResult.concat(arrangmrntBooks(response));
+        let books = arrangmrntBooks(response);
+        createBlock(books);
+    });
+});
+
 function modalContent(src) {
     document.querySelector('.modal-header').innerHTML = `<button class='close'
     data-dismiss='modal'>x</button>
@@ -111,7 +105,12 @@ function modalContent(src) {
 document.querySelector('#bookshelf').addEventListener('click', (e) => {
     if (e.target.getAttribute('class') === 'btn btn-info openModal') {
         let id = e.target.id;
-        modalContent(books[id].summary);
+        let idArea = [];
+        for (let i = 0; i < responseResult.length; i++) {
+            idArea.push(responseResult[i].id)
+        };
+        let index = idArea.indexOf(id);
+        modalContent(responseResult[index].summary);
         $('#modal').modal();
-    };
+    }
 }, false);
