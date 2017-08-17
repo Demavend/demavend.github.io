ece7af068f42c3efb69192a6d6b131dfd8eb9828
Demavend <demavend12345@gmail.com>
2017-08-17 13:16:03 +0300

    Fix "Clean", "Show more" and "Open modal" fuctions

--- index.js
+++ index.js
@@ -5,19 +5,15 @@ const fullUrl = (val, start, max) => {
     return (url);
 };
 const clean = () => {
-    $('div.panel-success').remove();
+    $('div.panel-success').parent().remove();
     document.querySelector('.showMore').style.display = 'none';
     document.querySelector('form.pagination').style.display = 'none';
     responseResult = [];
 };
-
 let responseResult = [];
 let serch;
 let startIndex = 0;
 let totalItems = 0;
-
-
-
 class Book {
     constructor(data) {
         this.books = {
@@ -39,7 +35,7 @@ class Book {
         newDiv.innerHTML = `<div class="panel panel-success"><div class="panel-heading"><h1 class="text-center">${this.books.summary.title}` +
             `</h1></div><div class="panel-body"><div class="col-md-2"><img src="${this.books.img}` +
             `"width="100%"  class="img-responsive"></img></div><div class="col-md-8"><h4 class="text-justify">${this.books.description}` +
-            `</h4></div><div class="col-md-2"><button class="btn btn-info openModal" id=${this.books.id}` +
+            `</h4></div><div class="col-md-2"><button class="btn btn-info openModal" data-id=${this.books.id}` +
             `>Show<br>summary</button></div></div></div>`;
         document.querySelector('#bookshelf').appendChild(newDiv);
     };
@@ -58,7 +54,6 @@ function getBooks(src) {
         xhr.send();
     });
 };
-
 document.getElementById('btnSerch').addEventListener('click', (e) => {
     serch = document.getElementById('bookName').value;
     if (document.querySelector('div.panel-success')) {
@@ -66,7 +61,6 @@ document.getElementById('btnSerch').addEventListener('click', (e) => {
     };
     getBooks(fullUrl(serch, startIndex, STEP)).then(response => {
         response.items.forEach((item) => {
-            console.log(item);
             let book = new Book(item);
             book.createBlock();
         });
@@ -74,17 +68,16 @@ document.getElementById('btnSerch').addEventListener('click', (e) => {
     document.querySelector('.showMore').style.display = 'block';
     document.querySelector('form.pagination').style.display = 'block';
 });
-
-
 document.getElementById('clean').addEventListener('click', (e) => {
     clean();
 });
 document.querySelector('.showMore').addEventListener('click', (e) => {
     startIndex += STEP;
     getBooks(fullUrl(serch, startIndex, STEP)).then(response => {
-        responseResult = responseResult.concat(arrangmrntBooks(response));
-        //let books = arrangmrntBooks(response);
-        createBlock(books);
+        response.items.forEach((item) => {
+            let book = new Book(item);
+            book.createBlock();
+        });
     });
 });
 
@@ -92,21 +85,18 @@ function modalContent(src) {
     document.querySelector('.modal-header').innerHTML = `<button class='close'
     data-dismiss='modal'>x</button>
     <h4 class='text-center text-primary'>${src.title}</h4>`;
-    //let book = '';
+    let bookSummary = '';
     for (let key in src) {
-        //book += `<span class='capitalize'>${key}</span>:  ${src[key]};<br>`;
+        bookSummary += `<span class='capitalize'>${key}</span>:  ${src[key]};<br>`;
     };
-    document.querySelector('#bodyModal').innerHTML = book;
+    document.querySelector('#bodyModal').innerHTML = bookSummary;
 };
 document.querySelector('#bookshelf').addEventListener('click', (e) => {
     if (e.target.getAttribute('class') === 'btn btn-info openModal') {
-        let id = e.target.id;
-        let idArea = [];
-        for (let i = 0; i < responseResult.length; i++) {
-            idArea.push(responseResult[i].id)
-        };
-        let index = idArea.indexOf(id);
-        modalContent(responseResult[index].summary);
+        let idItem = responseResult.find((item) => {
+            return item.id === e.target.getAttribute('data-id');
+        });
+        modalContent(idItem.summary);
         $('#modal').modal();
     }
 }, false);
